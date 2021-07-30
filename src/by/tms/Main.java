144df104df263e54efb41741144d53b16fba0c3e
BeginnerAnatolij <creativ.by@gmail.com>
2021-07-30 23:19:05 +0300

    HomeTask2

---
+++ src/by/tms/Main.java
@@ -0,0 +1,14 @@
+package by.tms;
+
+public class Main {
+    public static void main(String[] args) {
+        Rectangle rectangleTwo = new Rectangle();
+        Rectangle rectangleOne = new Rectangle(2, 4, 5, 2);
+        double area = rectangleOne.square();
+        System.out.println(area);
+        double perimiterone = rectangleOne.perimeter();
+        System.out.println(perimiterone);
+        double diagonalone = rectangleOne.diagonal();
+        System.out.println(diagonalone);
+    }
+}

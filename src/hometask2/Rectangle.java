b63c9e1e38cf51435b1e88b6b62bf0b481b7bd98
BeginnerAnatolij <87751340+BeginnerAnatolij@users.noreply.github.com>
2021-07-31 15:00:38 +0300

    HomeTask2

---
+++ src/hometask2/Rectangle.java
@@ -0,0 +1,43 @@
+package hometask2;
+
+import hometask2.Point;
+
+public class Rectangle {
+    public Point leftTop;
+    public Point bottomRight;
+
+    public Rectangle(Point leftTop, Point bottomRight) {
+        this.leftTop = leftTop;
+        this.bottomRight = bottomRight;
+    }
+
+    public Rectangle(double x1, double y1, double x2, double y2) {
+        leftTop = new Point(x1, y1);
+        bottomRight = new Point(x2, y2);
+    }
+
+    public Rectangle() {
+        System.out.println("Вызывается конструктор без параметров");
+    }
+
+    public double square() {
+        double leftSide = Math.abs(bottomRight.getX() - leftTop.getX());
+        double bottomSide = Math.abs(bottomRight.getY() - leftTop.getY());
+        double square = leftSide * bottomSide;
+        return square;
+    }
+
+    public double perimeter() {
+        double leftSide = Math.abs(bottomRight.getX() - leftTop.getX());
+        double bottomSide = Math.abs(bottomRight.getY() - leftTop.getY());
+        double perimeter = Math.abs(2 * (leftSide + bottomSide));
+        return perimeter;
+    }
+
+    public double diagonal() {
+        double leftSide = Math.abs(bottomRight.getX() - leftTop.getX());
+        double bottomSide = Math.abs(bottomRight.getY() - leftTop.getY());
+        double diagonal = Math.sqrt(Math.abs((leftSide * leftSide + bottomSide * bottomSide)));
+        return diagonal;
+    }
+}
